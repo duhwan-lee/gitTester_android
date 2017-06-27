5893885a5a3e1878084c2be5b83d0860de1fd6ee
doohwanlee <enghks1414@gmail.com>
2017-06-27 10:55:34 +0900

    add log branch2

--- app/src/main/java/com/example/doohwanlee/gittester/MainActivity.java
+++ app/src/main/java/com/example/doohwanlee/gittester/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("ldh","log");
+        Log.i("ldh","Add Branch1");
+        Log.i("ldh","Add Branch2");
     }
 }
